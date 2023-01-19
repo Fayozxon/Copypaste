461481d52b9b5fc4c2165b0be8cdb363c1f3e5ea
Fayozxon <83503559+Fayozxon@users.noreply.github.com>
2023-01-19 13:48:44 +0500

    yangi bo'lim qo'shildi

--- js/app.js
+++ js/app.js
@@ -6,6 +6,16 @@ let text = document.getElementById('text');
 let iterationNum = document.getElementById('iterationNum');
 let actionBtn = document.getElementById('actionBtn');
 
+function checkInputs() {
+    if (iterationNum.value && text.value && iterationNum.value <= 10000) {
+        iterateText();
+    } else if (text.value && iterationNum.value > 10000) {
+        alert('Son 10,000 dan katta bo\'la olmaydi!');
+    } else {
+        alert('So\'z va qaytarilishlar sonini kiritganingizga ishonch hosil qiling!');
+    }
+}
+
 function iterateText() {
     let text0 = '';
     for (let i = 1; i <= iterationNum.value; i++) {
@@ -24,15 +34,22 @@ function iterateText() {
         text2 += (`${i} ${text.value}\n`);
     }
     styles[2].querySelector('.style-2').textContent = text2;
+    text.value = '';
 }
 
 iterateText();
-actionBtn.addEventListener('click', iterateText);
+actionBtn.addEventListener('click', checkInputs);
 
 // Styles Copy Scripts
 for (let i = 0; i < styles.length; i++) {
-    styles[i].querySelector('.style-btn-'+i).addEventListener('click', function() {
+    let btn = styles[i].querySelector('.style-btn-'+i);
+    btn.addEventListener('click', function() {
         styles[i].querySelector('.style-'+i).select();
         document.execCommand('copy');
+
+        btn.classList.add('active');
+        btn.addEventListener('animationend', function() {
+            btn.classList.remove('active');
+        })
     });
 }
